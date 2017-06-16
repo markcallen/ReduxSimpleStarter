c87af3e0eed04ee8ff28a62d734f452ef63d9bdd
Mark C Allen <mark@markcallen.com>
2017-06-16 16:55:33 -0700

    End of Section 1 - Lecuture 20

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyDTfpgg7sYNWxLyi0U2acM762dSS0AhcVQ';
+
 // Create a new component
 // This component will produce some html
-const App = () => <div>Hi!</div>;
+const App = () =>
+    <div>
+      <SearchBar />
+    </div>
+  ;
 
 // Take this comonents generated html and put it on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
